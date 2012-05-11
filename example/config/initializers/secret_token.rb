dc55dc39dfda298bd9ac05f451ed2d87b1b30b9e
Nick Giancola <nick@gophilosophie.com>
2012-05-11 11:51:17 -0700

    Removes example rails app cause its totally not functional

--- example/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-NestedAttributesExample::Application.config.secret_token = '48a92dc8c3d289e317771188f9329562f8548bd3ea8d7d29e57a7a67252ec98dc36b51f935b14d7b56f03eaf1ade7f914a0c702aa8532c18403076ebe1ed2eb2'
